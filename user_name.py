66d9319e10f925922047bb7a409c5410ff6d7571
oda91-7 <yrhr.ldh.11@icloud.com>
2023-07-02 15:31:04 +0900

    self_introductionメソットの追加

--- user_name.py
+++ user_name.py
@@ -1,13 +1,19 @@
-class UserName:
+class User:
     def __init__(self, name):
         if not (4 <= len(name) <= 20):
             raise ValueError(f"{name}はルール違反です")
         self.name = name
+
     def screen_name(self):
         return self.name.upper()
 
+    def self_introduction(self):
+        return f"私の名前は{self.name.upper()}です"
+
+
 # UserNameクラスのインスタンス化
-tanaka = UserName(name="tanaka")
+tanaka = User(name="tanaka")
 
 # 出力
 print(tanaka.screen_name())
+print(tanaka.self_introduction())
